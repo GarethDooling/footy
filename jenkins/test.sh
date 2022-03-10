16ced580b975f42a80aa5cbc152b80ecb0e45907
Gareth Dooling <garethdooling@yahoo.co.uk>
2022-03-10 12:08:25 +0000

    GD

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,7 +3,7 @@
 source venv/bin/activate
 
 python3 -m pytest \
-	--cov=application \
+	--cov=app \
     --cov-report term-missing \
     --cov-report xml:coverage.xml \
     --junitxml=junit_report.xml
