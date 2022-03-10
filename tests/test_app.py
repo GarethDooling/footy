9dda9745b5c089b757fd9e8a4d7ce0f83a13d67e
Gareth Dooling <garethdooling@yahoo.co.uk>
2022-03-10 17:11:28 +0000

    GD: Give up

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,55 +6,54 @@ from flask import url_for
 class TestBase(TestCase):
     def create_app(self):
         app.config.update(SQLALCHEMY_DATABASE_URI='sqlite:///',
-            DEBUG=True,
-            WTF_CSRF_ENABLES=False
-            )
+                SECRET_KEY='SECRET_KEY',
+                DEBUG=True,
+                WTF_CSRF_ENABLES=False 
+                )
         return app
 
     def setUp(self):
-        db.drop_all()
         db.create_all()
 
-        task1 = Teams(name="new task", description= "this new task")
+        team1 = Teams(name="new team", description= "this new team")
 
-        db.session.add(task1)
+        db.session.add(team1)
         db.session.commit()
 
     def tearDown(self):
-        # Will be called after every test
         db.drop_all()
 
 class TestCRUD(TestBase):
 
-    def test_read_tasks(self):
+    def test_read_teams(self):
         response = self.client.get(url_for('read'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('new task', str(response.data))
-        self.assertIn('this new task', str(response.data))
+        self.assertIn('new team', str(response.data))
+        self.assertIn('this new team', str(response.data))
 
-    def test_create_tasks(self):
+    def test_create_teams(self):
         response = self.client.post(
             url_for('create'),
-            data=dict(name="created task", description="this is a create task"),
+            data=dict(name="created team", description="this is a create team"),
             follow_redirects=True
         )
-        created_task = Teams.query.get(2)
-        self.assertEqual(created_task.name, "created task")
-        self.assertIn('created task', str(response.data))
-        self.assertIn('this is a create task', str(response.data))
+        created_team = Teams.query.get(2)
+        self.assertEqual(created_team.name, "created team")
+        self.assertIn('created team', str(response.data))
+        self.assertIn('this is a create team', str(response.data))
 
-    def test_update_tasks(self):
+    def test_update_teams(self):
         response = self.client.post(
-            url_for('update_task', id=1),
+            url_for('update_team', id=1),
             data={"description": "Testing update functionality"},
             follow_redirects=True
         )
-        self.assertIn("updated task", str(response.data))
-        self.assertIn("this is an updated task", str(response.data))
+        self.assertIn("updated team", str(response.data))
+        self.assertIn("this is an updated team", str(response.data))
 
-    def test_delete_tasks(self):
+    def test_delete_teams(self):
         response = self.client.post(
-            url_for('delete', name="new task"),
+            url_for('delete', name="new team"),
             follow_redirects=True
         )
-        self.assertNotIn("new task", str(response.data))
+        self.assertNotIn("new team", str(response.data))
